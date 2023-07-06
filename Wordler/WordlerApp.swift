f38771de9298bcae4d61a9db0e408cd6cc574b95
Danilka <danilkacrypto111@gmail.com>
2023-07-06 15:29:54 +0300

    Initial Commit

---
+++ Wordler/WordlerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordlerApp.swift
+//  Wordler
+//
+//  Created by danilka on 06.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordlerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
